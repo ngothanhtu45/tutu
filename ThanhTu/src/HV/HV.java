84b0d8e9a2b5860361f9125f3104ca6dc2390a2c
ADMIN <ADMIN@192.168.1.6>
2020-07-29 21:23:00 +0700

    bài nhóm nè

---
+++ ThanhTu/src/HV/HV.java
@@ -0,0 +1,21 @@
+package HV;
+import java.util.Scanner;
+public class HV {
+	protected int chieuDaiCanh;
+    public static double tinhDienTich(double chieuDaiCanh) {
+        return chieuDaiCanh * chieuDaiCanh;
+    }
+    public static double tinhChuVi(double chieuDaiCanh){
+        return chieuDaiCanh * 4;
+    }
+    public static void main(String[] args) {
+        double canh;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập chiều dài cạnh hình vuông: ");
+        canh = scanner.nextDouble();
+        System.out.println("Diện tích hình vuông là: " + tinhDienTich(canh));
+        System.out.println("Chu vi hình vuông là: "+ tinhChuVi(canh));
+        System.out.println("----------------------------");
+        System.out.println("Chương trình nay được đăng tại Freetuts.net");
+    }
+}
